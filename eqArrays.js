e526ca8335cb41ad809062e7792b7b8d498d8c66
Morsal Niyaz <morsalniyaz@gmail.com>
2021-10-27 09:26:24 -0700

    Edited

--- eqArrays.js
+++ eqArrays.js
@@ -15,6 +15,10 @@ const eqArrays = function(arrayOne, arrayTwo) {
     return false;
   }
 
+  if (arrayOne == null || arrayTwo == null) {
+    return false;
+  }
+
   for (let i = 0; i < arrayOne.length; i++) {
     if (arrayOne[i] !== arrayTwo[i]) {
       return false;
@@ -24,22 +28,8 @@ const eqArrays = function(arrayOne, arrayTwo) {
 };
 
 
-// function eqArrays(a, b) {
-//   if (a === b) return true;
-//   if (a == null || b == null) return false;
-//   if (a.length !== b.length) return false;
-
-//   // If you don't care about the order of the elements inside
-//   // the array, you should sort both arrays here.
-//   // Please note that calling sort on an array will modify that array.
-//   // you might want to clone your array first.
-
-//   for (var i = 0; i < a.length; ++i) {
-//     if (a[i] !== b[i]) return false;
-//   }
-//   return true;
-// }
-
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([1, 2, 56], [1, 2]), false);
