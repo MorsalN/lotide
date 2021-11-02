b1abdb85579aa2cf24f09d8b326bd6048ef28cab
Morsal Niyaz <morsalniyaz@gmail.com>
2021-11-01 18:26:18 -0700

    deleted comments

--- head.js
+++ head.js
@@ -8,8 +8,3 @@ const head = function(array) {
 
 module.exports = head;
 
-//TEST CODE
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([]), 5);
-// assertEqual(head([5]), 5);
