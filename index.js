ba89c42eb1eec2f1383438e9ef94cf64745f29b6
Morsal Niyaz <morsalniyaz@gmail.com>
2021-11-01 19:58:24 -0700

    Added all utility functions

---
+++ index.js
@@ -0,0 +1,43 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+
+const countLetters   = require('./countLetters');
+const eqArrays   = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+
+  countLetters: countLetters,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+
+  letterPositions: letterPositions,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+
+  map: map,
+  takeUntil: takeUntil,
+  without: without,
+};
